e0178ab898e40428186fa94456edac2c7f942209
Marius K <marius.into@gmail.com>
2022-01-10 10:31:47 +0200

    Add state and methods to view

--- src/taskpane/taskpane.js
+++ src/taskpane/taskpane.js
@@ -13,12 +13,78 @@ import settings from "../components/settings.js"
 
 let view = {
 
-	addLinks: false,
-	actions: ["click", "change"],
-	avatar: "../../assets/avatar.png",
-	pageTitle: "Onenote",
-	pageUrl: location.href,
-	tasks: [],
+	state: {
+		addLinks: false,
+		avatar: "../../assets/avatar.png",
+		pageTitle: "Onenote",
+		pageUrl: location.href,
+		tasks: [],
+	},
+
+	listeners: ["click", "change"],
+
+	methods: {
+
+		connect() {
+			let token = view.getValue("token")
+			if (token.length > 0) todoist.sync(token)
+		},
+	
+		disconnect() {
+			localStorage.removeItem('todoist_token')
+			todoist.token = 'none'
+			Office.context.ui.closeContainer()
+		},
+	
+		toggle() {
+			view.state.addLinks = !view.state.addLinks
+		},
+	
+		refresh() {
+			let projects = view.get("projects"),
+			title = projects.selectedOptions[0].text,
+			tasks = view.state.tasks.length + " new task(s)"
+	
+			if (projects.value == "new") {
+				title = view.state.pageTitle
+				view.update({ title, tasks })
+			
+			} else {
+				todoist.getData(projects.value).then(data => {
+					let items = [], todos = []
+					data.items.forEach( item => items.push(tools.getText(item.content)) )
+					todos = view.state.tasks.filter( todo => !items.includes(todo) )
+					tasks = todos.length + " new task(s)"
+					view.update({ title, tasks })
+				})
+			}
+				
+		},
+	
+		push() {
+			let projectId = view.getValue('projects')
+	
+			if (projectId == "new") {
+				todoist.push(view.state.tasks)
+	
+			} else {
+				todoist.getData(projectId).then(data => {
+					let items = [], tasks = []
+					data.items.forEach( item => items.push(tools.getText(item.content)) )
+					tasks = view.state.tasks.filter( todo => !items.includes(todo) )
+					todoist.push(tasks, projectId)
+				})
+			}
+		},
+	
+		retry() {
+			getPageTasks()
+		},
+	
+		close() {
+			Office.context.ui.closeContainer()
+		},
+	},
 
 	get(id) {
 		return document.getElementById(id)
@@ -48,10 +114,10 @@ let view = {
 	show(component, data) {
 		this.get("app-body").innerHTML = component.template
 
-		view.actions.forEach( action => {
+		view.listeners.forEach( action => {
 			let elements = this.get('app-body').querySelectorAll(`[${action}]`)
 			elements.forEach( el => {
-				el.addEventListener(action, view[el.getAttribute(action)])
+				el.addEventListener(action, view.methods[el.getAttribute(action)])
 			})
 		})
 
@@ -64,66 +130,6 @@ let view = {
 
 	alert(title, details = "unknown") {
 		this.show(alert, { title, details })
-	},
-
-	connect() {
-		let token = view.getValue("token")
-		if (token.length > 0) todoist.sync(token)
-	},
-
-	disconnect() {
-		localStorage.removeItem('todoist_token')
-		todoist.token = 'none'
-		view.close()
-	},
-
-	toggle() {
-		view.addLinks = !view.addLinks
-	},
-
-	refresh() {
-		let projects = view.get("projects"),
-		title = projects.selectedOptions[0].text,
-		tasks = view.tasks.length + " new task(s)"
-
-		if (projects.value == "new") {
-			title = view.pageTitle
-			view.update({ title, tasks })
-		
-		} else {
-			todoist.getData(projects.value).then(data => {
-				let items = [], todos = []
-				data.items.forEach( item => items.push(tools.getText(item.content)) )
-				todos = view.tasks.filter( todo => !items.includes(todo) )
-				tasks = todos.length + " new task(s)"
-				view.update({ title, tasks })
-			})
-		}
-			
-	},
-
-	push() {
-		let projectId = view.getValue('projects')
-
-		if (projectId == "new") {
-			todoist.push(view.tasks)
-
-		} else {
-			todoist.getData(projectId).then(data => {
-				let items = [], tasks = []
-				data.items.forEach( item => items.push(tools.getText(item.content)) )
-				tasks = view.tasks.filter( todo => !items.includes(todo) )
-				todoist.push(tasks, projectId)
-			})
-		}
-	},
-
-	retry() {
-		getPageTasks()
-	},
-
-	close() {
-		Office.context.ui.closeContainer()
 	},
 
 },
@@ -160,12 +166,12 @@ todoist = {
 				localStorage.setItem("todoist_token", todoist.token)
 
 				view.show(settings, {
-					avatar: data.user.avatar_medium || view.avatar,
+					avatar: data.user.avatar_medium || view.state.avatar,
 					user: data.user.full_name,
 					mail: data.user.email.toLowerCase(),
-					tasks: view.tasks.length + " task(s)",
-					title: view.pageTitle,
-					preview: view.tasks[0].substring(0, 34),
+					tasks: view.state.tasks.length + " task(s)",
+					title: view.state.pageTitle,
+					preview: view.state.tasks[0].substring(0, 34),
 				})
 
 				let list = view.get("projects")
@@ -176,7 +182,7 @@ todoist = {
 				let projects = view.get("dropdown")
 				new fabric['Dropdown'](projects)
 
-				if (view.addLinks) {
+				if (view.state.addLinks) {
 					let links = view.get("add-links")
 					if (links != null) links.classList.add("is-selected")
 				}
@@ -189,42 +195,41 @@ todoist = {
 	},
 
 	push(tasks = [], id = "new") {
-		let project = { id, tasks },
-		project_id = parseInt(project.id) || todoist.uuid(),
-		commands = [],
 
-		headers = {
-			'Authorization': 'Bearer ' + this.token,
-			'Content-Type': 'application/json'
-		},
+		if (tasks.length > 0) {
+			
+			let project = { id, tasks },
+			project_id = parseInt(project.id) || todoist.uuid(),
+			commands = [],
 
-		message = "Proccesing..."
-		view.show(status, { message })
+			headers = {
+				'Authorization': 'Bearer ' + this.token,
+				'Content-Type': 'application/json'
+			},
 
-		if (project.id == "new") {
-			commands.push({
-				type: "project_add",
-				temp_id: project_id,
-				uuid: todoist.uuid(),
-				args: { name: view.pageTitle }
+			message = "Proccesing..."
+			view.show(status, { message })
+
+			if (project.id == "new") {
+				commands.push({
+					type: "project_add",
+					temp_id: project_id,
+					uuid: todoist.uuid(),
+					args: { name: view.state.pageTitle }
+				})
+			} 
+
+			tasks.forEach( todo => {
+				let link = view.state.pageUrl,
+				content = view.state.addLinks ? `[${todo}](${link})` : todo
+				commands.push({
+					type: "item_add",
+					temp_id: todoist.uuid(),
+					uuid: todoist.uuid(),
+					args: { content, project_id }
+				})
 			})
-		} 
-
-		tasks.forEach( todo => {
-			let content = view.addLinks ? `[${todo}](${view.pageUrl})` : todo
-			commands.push({
-				type: "item_add",
-				temp_id: todoist.uuid(),
-				uuid: todoist.uuid(),
-				args: { content, project_id }
-			})
-		})
-
-		if (commands.length == 0) {
-			view.alert("No new tasks found.", 
-					   "No new tasks found on this page!")
-
-		} else {
+			
 			fetch(todoist.url, { 
 				method: 'POST',
 				headers : headers,
@@ -235,13 +240,17 @@ todoist = {
 				res.json().then(data => {
 					let projectId = data.temp_id_mapping[project_id] || project_id
 					window.open("https://todoist.com/showProject?id=" + projectId, "_blank")
-					view.close()
+					Office.context.ui.closeContainer()
 				})
 			})
 
 			.catch(error => {
 				view.alert("Export failed!", error);
 			})
+
+		} else {
+			view.alert("No new tasks found.", 
+					   "No new tasks found on this page!")
 		}
 
 	},
@@ -284,7 +293,7 @@ Office.onReady((info) => {
 
 export async function getPageTasks() {
 
-	view.tasks = []
+	view.state.tasks = []
 	view.show(status)
 	OneNote.run(context => {
 
@@ -299,8 +308,8 @@ export async function getPageTasks() {
 		return context.sync()
 
 		.then(() => {
-			view.pageTitle = page.title
-			view.pageUrl = page.webUrl
+			view.state.pageTitle = page.title
+			view.state.pageUrl = page.webUrl
 			page.contents.items.forEach(item => {
 				if (item.type == 'Outline') {
 					item.load("outline/paragraphs, outline/paragraphs/type")
@@ -345,11 +354,11 @@ export async function getPageTasks() {
 			strings.forEach( html => {
 				let doc = parser.parseFromString(html.value, 'text/html'),
 				tag = doc.querySelector("[data-tag=to-do]")
-				if (tag != null) view.tasks.push(tag.innerText.trim())
+				if (tag != null) view.state.tasks.push(tag.innerText.trim())
 			})
-			console.log('tasks found:', view.tasks.length)
+			console.log('tasks found:', view.state.tasks.length)
 			
-			if (view.tasks.length == 0)
+			if (view.state.tasks.length == 0)
 				view.alert("No tasks found! There is nothing to export.", 
 						   "No to-do tags found on this page!")
 			else if (todoist.token == "none") view.show(connect)
